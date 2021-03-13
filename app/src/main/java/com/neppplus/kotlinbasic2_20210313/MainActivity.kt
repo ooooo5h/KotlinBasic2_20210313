b319bfbb88bcd6688dba6475ab57b19971ae8008
전은형 <ooooo5h91@gmail.com>
2021-03-13 10:46:22 +0900

    문법(조건문 if까지 설명함)

--- app/src/main/java/com/neppplus/kotlinbasic2_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic2_20210313/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.kotlinbasic2_20210313
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+//            IF문 연습 코드
+
+
+        val myAge = 34
+
+        if (myAge >= 30) {
+
+            Log.d("나이", "30대 이상")
+
+        }
+
+        else if (myAge >= 20) {
+            Log.d("나이", "20대")
+
+        }
+
+
+        else {
+            Log.d("나이", "미성년자")
+        }
+
+
+
         sendBtn.setOnClickListener {
 
 
 //            버튼이 눌리면,
 //            1. inputEdt에 적힌 내용을 => inputMessage변수에 메모해라.
 
-            val inputMessage = inputEdt.text.toString()
+            val inputMessage = inputEdt.text.toString()  // val 변수 = 오르녹 : String
 
 //            2. contentTxt의 text속성값을 => inputMessage에 적힌 값으로 변경:set
 
